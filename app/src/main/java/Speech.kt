f4b49fded4b421148fba9d320d913cb788549490
Sean Miller <sj2mille@edu.uwaterloo.ca>
2018-07-31 22:20:08 -0400

    Improved file structure

--- app/src/main/java/Speech.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Created by sean_ on 2018-07-31.
- */
-
-class SpeechService {
-    private fun Speak() {
-        // stuff??
-    }
-}
---
+++ app/src/main/java/sean/miller/alohaandroid/Speech.kt
@@ -0,0 +1,9 @@
+package sean.miller.alohaandroid
+
+import    android.speech.tts.TextToSpeechService
+
+class SpeechService {
+    private fun Speak() {
+        // stuff??
+    }
+}
